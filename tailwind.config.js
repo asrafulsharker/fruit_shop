1108e6e26512701be632b12f4dfeafafe8b37888
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2023-07-25 22:53:00 -0700

    added navbar and make responsive

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    //...
+    plugins: [require("daisyui")],
+    theme: {
+        extend: {
+          
+        }
+      },
+      fontWeight: {
+        thin: '100',
+        hairline: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        'extra-bold': '800',
+        black: '900',
+      },
+      colors: {
+        'primary-bg-color': '#243c5a',
+      },
+  }
